b08519ca73850c09077869712a146c98fecbb4e3
Minhchau Dang <minhchau.dang@liferay.com>
2018-09-05 15:59:36 -0700

    If AUTHORIZATION_URL is not set, don't lock down the application

--- app.js
+++ app.js
@@ -14,14 +14,16 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(
-	basicAuth({
-		authorizer: liferayAuthorization,
-		authorizeAsync: true,
-		challenge: 'true',
-		realm: "JIRA Dashboard"
-	})
-);
+if (process.env.AUTHORIZATION_URL) {
+  app.use(
+    basicAuth({
+      authorizer: liferayAuthorization,
+      authorizeAsync: true,
+      challenge: 'true',
+      realm: "JIRA Dashboard"
+    })
+  );
+}
 
 app.use('/', index);
 
